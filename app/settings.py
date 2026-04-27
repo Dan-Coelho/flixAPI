fb36589034f802fb1ece850ba2a135e229e66f30
Daniel <danpoco@gmail.com>
2026-04-27 10:01:40 -0300

    prod: alteração settings

--- app/settings.py
+++ app/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ajk-^n*2l@q0p&_245q#r%&xw#ch%rj82=u_ko1qp1m!n8xm&x"
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
